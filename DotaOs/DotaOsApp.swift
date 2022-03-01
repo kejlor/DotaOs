a5caa8c46275b219c3d5088fa6b6d3f0b0a30c9d
kejlor <woj450@gmail.com>
2022-03-02 00:45:04 +0100

    Initial Commit

---
+++ DotaOs/DotaOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaOsApp.swift
+//  DotaOs
+//
+//  Created by Bartosz Wojtkowiak on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DotaOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
